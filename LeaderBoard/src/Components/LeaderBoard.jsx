e1eb422598f6c1c74378042220595b51b0cc8329
Rayudu03 <vinayrayudu7865@gmail.com>
2025-08-10 23:22:43 +0530

    Update LeaderBoard.jsx

--- LeaderBoard/src/Components/LeaderBoard.jsx
+++ LeaderBoard/src/Components/LeaderBoard.jsx
@@ -13,7 +13,7 @@ function LeaderBoard() {
   const[collection,setCollection]  = useState([]);
 
   useEffect(()=>{
-    socket.current = io('http://localhost:5000');
+    socket.current = io('https://quiz-admin-gr40.onrender.com');
     socket.current.on('connect',()=>{
       console.log('Main screen '+socket.current.id);
     });
