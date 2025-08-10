4a8d80ccba385a24f5ed300f722e78f0bd7f4b83
Rayudu03 <vinayrayudu7865@gmail.com>
2025-08-10 23:40:22 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -12,7 +12,7 @@ const {QuestionCollections, connectDB} = require('./db.js');
 
 const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:5173','http://localhost:5174','http://localhost:5175'],  // client runs on 5173 Admin runs on 5174
+    origin: "*",
     methods: ['GET', 'POST']
   }
 });
